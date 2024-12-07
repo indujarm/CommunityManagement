28b2efc649ad59a71a2cebf9de3dde3136971fc9
ramprasad <ramprasadsunkara008@gmail.com>
2024-12-08 00:09:35 +0530

    updated module 1

---
+++ module1/src/main/java/com/infosys/community/dto/LoginDto.java
@@ -0,0 +1,9 @@
+package com.infosys.community.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+    private String email;
+    private String password;
+}
--- module1/src/main/java/com/infosys/community/repository/ResidentDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infosys.community.repository;
-
-import com.infosys.community.model.ResidentDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ResidentDetailsRepository extends JpaRepository<ResidentDetails,Long> {
-}
